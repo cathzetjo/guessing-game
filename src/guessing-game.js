8f6d4da51ff72324e52cad93ea61389cf1cb9761
Katerina Zhadko <katerinkaj@icloud.com>
2021-09-06 23:55:52 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.play = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.play;
     }
 
     greater() {
-
+        this.min = this.play;
     }
 }
 
